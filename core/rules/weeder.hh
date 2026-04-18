22897b7536cf2e32843abc37636c8ac3fd32d0f8
ua880d <root@redlinux.Home>
2026-04-18 04:27:56 +0200

    Fix ScoreHit declaration order in weeder header

--- core/rules/weeder.hh
+++ core/rules/weeder.hh
@@ -10,11 +10,24 @@ using namespace std;
 
 class Weeder
 {
+public:
+  struct ScoreHit
+    {
+      std::string expression;
+      int score_delta;
+      int is_negative;
+      int matched;
+      std::string header_tag;
+      std::string header_body;
+      int normalized_subject;
+    };
+  
 private:
   vector<string> msg_ids;
 
   int last_score;
   string last_decision;
+  std::vector<ScoreHit> last_score_hits; 
 
   int check_duplicates (Header*);
   int check_maxlength (Header*) const;
@@ -22,21 +35,8 @@ private:
   int check_deny_rules (Header*) const;
   int check_scores (Header*);
 
-  std::vector<ScoreHit> last_score_hits;
 
 public:
-
-
-  struct ScoreHit
-  {
-    std::string expression;
-    int score_delta;
-    int is_negative;
-    int matched;
-    std::string header_tag;
-    std::string header_body;
-    int normalized_subject;
-  };
 
   Weeder () : last_score (0), last_decision ("pass") {}
 
@@ -45,6 +45,6 @@ public:
   int final_score (void) const { return last_score; }
   const string& decision (void) const { return last_decision; }
   const std::vector<ScoreHit>& score_hits (void) const { return last_score_hits; }
-};
 
+};
 #endif
